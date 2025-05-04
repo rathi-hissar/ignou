53ee63e6026484757071a2931acff83bc4c02fdd
rathi-hissar <rathi.hissar@gmail.com>
2025-05-04 09:37:23 -0700

    BubbleSort

---
+++ IgnouLabs/FirstSemLabs/BubbleSort.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace FirstSemLabs
+{
+    public class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // Swap elements
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IgnouLabs/FirstSemLabs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace FirstSemLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers separated by spaces:");
+            string input = Console.ReadLine();
+
+            // Parse input into an array of integers
+            int[] numbers = input.Split(' ')
+                                 .Where(x => int.TryParse(x, out _))
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            Console.WriteLine("Original Array: " + string.Join(", ", numbers));
+
+            // Perform Bubble Sort
+            BubbleSort.Sort(numbers);
+
+            Console.WriteLine("Sorted Array: " + string.Join(", ", numbers));
+        }
+    }
+}
